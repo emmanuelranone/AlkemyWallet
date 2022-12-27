eaca0f2d019925e0dc94c187c8dd3a98d7815e90
Olivera Leonardo <oliveraleonardoezequiel@gmail.com>
2022-12-27 17:10:19 -0300

    Add Entity Base and Role

---
+++ AlkemyWallet/Entities/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlkemyWallet.Entities;
+
+public class EntityBase
+{
+    [Key]
+    public int Id { get; set; }
+}
