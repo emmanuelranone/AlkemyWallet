62b527824b702b3d89903be985b0cdd96ac03d57
Diego Balderrama <84107767+diego-balde@users.noreply.github.com>
2022-12-27 18:11:21 -0300

    Add DbContext and SeedUsers

--- AlkemyWallet/DataAccess/WalletDbContext.cs
+++ AlkemyWallet/DataAccess/WalletDbContext.cs
@@ -1,6 +1,24 @@
-﻿namespace AlkemyWallet.DataAccess
+﻿using AlkemyWallet.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AlkemyWallet.DataAccess
 {
-    public class WalletDbContext
+    public class WalletDbContext : DbContext
     {
+        public WalletDbContext(DbContextOptions<WalletDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // seed roles acá
+            modelBuilder.SeedUsers();
+        }
     }
 }
