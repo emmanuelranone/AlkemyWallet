365f9c6a4ca3217083ea859bd083cb0e7bcae851
Matias <mgabrielpalacio@gmail.com>
2022-12-27 17:34:42 -0300

    Added Entity Added Account Entity

---
+++ AlkemyWallet/Entities/Account.cs
@@ -0,0 +1,18 @@
+﻿using AlkemyWallet.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eWallet_API.Entities
+{
+    public class Account : EntityBase
+    {
+        [Column("creationDate")]
+        public DateTime CreationDate { get; set; }
+        [Column("money")]
+        public double Money { get; set; }
+        [Column("isBlocked")]
+        public bool IsBlocked { get; set; }
+        [ForeignKey("User")]
+        public int User_Id { get; set; }
+        public virtual User User { get; set; }
+    }
+}
