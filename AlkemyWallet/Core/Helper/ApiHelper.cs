fa7e24340e05c842e8ba04a30290201d94f21004
emmanuelranone <55627399+emmanuelranone@users.noreply.github.com>
2022-12-27 17:35:17 -0300

    Revert "Crear entidad y migraciòn de Transactions"

---
+++ AlkemyWallet/Core/Helper/ApiHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace AlkemyWallet.Core.Helper
+{
+    public class ApiHelper
+    {
+    }
+}
--- AlkemyWallet/Program.cs
+++ AlkemyWallet/Program.cs
@@ -1,3 +1,6 @@
+using AlkemyWallet.Core.Interfaces;
+using AlkemyWallet.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
