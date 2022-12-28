87a8d2c3ce4618d91c01a7dcd6af420867e9abee
Nico Ledesma <dev.ledesmanicolas@gmail.com>
2022-12-28 00:15:05 -0300

    Add dbContext, DbSet<Transaction> and TransactionSeed

--- AlkemyWallet/DataAccess/WalletDbContext.cs
+++ AlkemyWallet/DataAccess/WalletDbContext.cs
@@ -13,12 +13,15 @@ namespace AlkemyWallet.DataAccess
 
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<Transaction> Transactions { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // seed roles acá
             modelBuilder.SeedUsers();
+            // seed transactions
+            modelBuilder.SeedTransactions();
         }
     }
 }
