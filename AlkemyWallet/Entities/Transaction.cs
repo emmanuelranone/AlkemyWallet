5de15a2048b414ab5fe5be5554c7cc65ff7b7e82
Nico Ledesma <dev.ledesmanicolas@gmail.com>
2022-12-28 12:49:59 -0300

    Edit: Add column names

--- AlkemyWallet/Entities/Transaction.cs
+++ AlkemyWallet/Entities/Transaction.cs
@@ -1,26 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using AlkemyWallet.Entities;
 
 namespace AlkemyWallet.Entities
 {
     public class Transaction : EntityBase
     {
+        [Column("amount")]
         public decimal Amount { get; set; }
+
+        [Column("concept")]
         public string Concept { get; set; }
+
+        [Column("date")]
         public DateTime Date { get; set; }
+
+        [Column("type")]
         public string Type { get; set; }
-        public int Account_id { get; set; }
-        public int User_id { get; set; }
-        public int To_account_id { get; set; }
 
+        [Column("account_id")]
+        public int AccountId { get; set; }
 
-        [ForeignKey("Account_id")]
-        public virtual Account account { get; set; }
+        [Column("user_id")]
+        public int UserId { get; set; }
 
-        [ForeignKey("User_id")]
-        public virtual User user { get; set; }        
+        [Column("to_account_id")]
+        public int ToAccountId { get; set; }
+
+        
+        [ForeignKey("AccountId")]
+        public Account Account { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }        
     }
 }
