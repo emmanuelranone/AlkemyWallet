d1ef1b8144692389dc6f353e9dc93668baba0542
Olivera Leonardo <oliveraleonardoezequiel@gmail.com>
2022-12-30 15:12:28 -0300

    Add SwaggerDoc and AddAuthorization

--- AlkemyWallet/Program.cs
+++ AlkemyWallet/Program.cs
@@ -8,27 +8,56 @@ using AlkemyWallet.Core.Mapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-// For Entity Framework
-builder.Services.AddScoped<IRoleService, RoleService>();
-builder.Services.AddScoped<IAccountService, AccountService>();
-
-builder.Services.AddDbContext<WalletDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DevConnection")));
-
-
+builder.Services.AddDbContext<WalletDbContext>(options =>
+    options.UseSqlServer(configuration.GetConnectionString("DevConnection")));
 
 // Add services to the container.
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddAutoMapper(typeof(EntityMapper));
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<ITransactionService, TransactionService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//--- Adding Authentication for Swagger.
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Wallet", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,  //  SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[] {}
+        }
+    });
+});
 
 // Adding Authentication
 builder.Services.AddAuthentication(options =>
@@ -37,7 +66,6 @@ builder.Services.AddAuthentication(options =>
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 })
-
 // Adding Jwt Bearer
 .AddJwtBearer(options =>
 {
@@ -54,11 +82,20 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
-builder.Services.AddAutoMapper(typeof(EntityMapper));
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ITransactionService, TransactionService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin",
+        authBuilder =>
+        {
+            authBuilder.RequireRole("Admin");
+        });
+    options.AddPolicy("Regular",
+        authBuilder =>
+        {
+            authBuilder.RequireRole("Regular");
+        });
 
+});
 
 var app = builder.Build();
 
