ead0f9a630d2e97b46e37d510cc984f2e7381a17
Olivera Leonardo <oliveraleonardoezequiel@gmail.com>
2023-01-05 16:08:04 -0300

    Add ErrorHandlingMiddleware

---
+++ AlkemyWallet/Core/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace AlkemyWallet.Core.Middleware;
+
+public class ErrorHandlingMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            await context.Response.WriteAsync
+                (JsonConvert.SerializeObject
+                (new 
+                { 
+                    status = (int)HttpStatusCode.BadRequest, 
+                    errors = new[] { new {  error = ex.Message } } 
+                }));
+        }
+    }
+}
