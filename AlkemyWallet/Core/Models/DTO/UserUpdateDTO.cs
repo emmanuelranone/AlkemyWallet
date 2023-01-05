abbd935c96fec59633f62bc03a4b82584cd46573
Diego Balderrama <84107767+diego-balde@users.noreply.github.com>
2023-01-05 11:25:59 -0300

    Us-48 - User update endpoint

---
+++ AlkemyWallet/Core/Models/DTO/UserUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AlkemyWallet.Core.Models.DTO
+{
+    public class UserUpdateDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
